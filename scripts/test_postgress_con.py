d85ee40a79ac1077e4ec78e71c6fb0a0cf25b6a2
nidhir1 <nidhi.doit@gmail.com>
2025-08-21 23:20:14 -0400

    updated

--- scripts/test_postgress_con.py
+++ scripts/test_postgress_con.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import psycopg2
 from psycopg2 import OperationalError
 from myconfig import settings
