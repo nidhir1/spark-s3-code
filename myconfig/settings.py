631506af4811281547241d151a0c40db6f659b43
nidhir1 <nidhi.doit@gmail.com>
2025-08-21 23:24:14 -0400

    updated

--- myconfig/settings.py
+++ myconfig/settings.py
@@ -10,3 +10,6 @@ POSTGRES_URL = "jdbc:postgresql://localhost:5432/postgres"
 POSTGRES_USER = "tinitiate"
 POSTGRES_PASSWORD = "Tinitiate!23456"
 POSTGRES_DRIVER = "org.postgresql.Driver"
+POSTGRES_HOST="localhost"
+POSTGRES_PORT=5432
+POSTGRES_DB="postgres"
